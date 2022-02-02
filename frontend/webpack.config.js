6403f77176ddf59059737707ccfe155d7c1ec2ec
Tomamonomae <snabukraine@ukr.net>
2022-02-02 14:33:25 +0200

    add mobile and tablet breakpoints, rewirite css styles using sass

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,12 +1,11 @@
 const path = require('path')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
+const CopyPlugin = require("copy-webpack-plugin");
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const OptimizeCssAssetWebpackPlugin = require('optimize-css-assets-webpack-plugin')
 const TerserWebpackPlugin = require('terser-webpack-plugin')
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
-
 
 const isDev = process.env.NODE_ENV === 'development'
 const isProd = !isDev
@@ -76,45 +75,45 @@ const jsLoaders = () => {
     return loaders
 }
 
-const plugins = () => {
-    const base = [
-        new HTMLWebpackPlugin({
-            template: './index.html',
-            minify: {
-                collapseWhitespace: isProd
-            }
-        }),
-        new CleanWebpackPlugin(),
-        new CopyWebpackPlugin([{
-            from: path.resolve(__dirname, 'favicon.ico'),
-            to: path.resolve(__dirname, 'dist')
-        }, {
-            from: path.resolve(__dirname, 'src', 'images', 'img1.jpg'),
-            to: path.resolve(__dirname, 'dist')
-        }, {
-            from: path.resolve(__dirname, 'src', 'images', 'img4.jpg'),
-            to: path.resolve(__dirname, 'dist')
-        }, {
-            from: path.resolve(__dirname, 'src', 'images', 'img3.jpg'),
-            to: path.resolve(__dirname, 'dist')
-        }, {
-            from: path.resolve(__dirname, 'src', 'images', 'img5.jpg'),
-            to: path.resolve(__dirname, 'dist')
-        }, {
-            from: path.resolve(__dirname, 'src', 'images', 'img9.jpg'),
-            to: path.resolve(__dirname, 'dist')
-        }]),
-        new MiniCssExtractPlugin({
-            filename: filename('css')
-        })
-    ]
+// const plugins = () => {
+//     const base = [
+//         new HTMLWebpackPlugin({
+//             template: './index.html',
+//             minify: {
+//                 collapseWhitespace: isProd
+//             }
+//         }),
+//         new CleanWebpackPlugin(),
+//         new CopyWebpackPlugin({
+//             patterns: [{
+//                     from: path.resolve(__dirname, 'favicon.ico'),
+//                     to: path.resolve(__dirname, 'dist')
+//                 },
+//                 {
+//                     from: path.resolve(__dirname, 'src', 'images', 'img1.jpg'),
+//                     to: path.resolve(__dirname, 'dist')
+//                 },
+//                 {
+//                     from: path.resolve(__dirname, 'src', 'images', 'img3.jpg'),
+//                     to: path.resolve(__dirname, 'dist')
+//                 },
+//                 {
+//                     from: path.resolve(__dirname, 'src', 'images', 'img4.jpg'),
+//                     to: path.resolve(__dirname, 'dist')
+//                 },
+//             ]
+//         }),
+//         new MiniCssExtractPlugin({
+//             filename: filename('css')
+//         })
+//     ]
 
-    if (isProd) {
-        base.push(new BundleAnalyzerPlugin())
-    }
+//     if (isProd) {
+//         base.push(new BundleAnalyzerPlugin())
+//     }
 
-    return base
-}
+//     return base
+// }
 
 module.exports = {
     context: path.resolve(__dirname, 'src'),
@@ -135,14 +134,46 @@ module.exports = {
         hot: isDev
     },
     devtool: isDev ? 'source-map' : '',
-    plugins: plugins(),
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: './index.html',
+            minify: {
+                collapseWhitespace: isProd
+            }
+        }),
+        new CleanWebpackPlugin(),
+        new CopyPlugin([{
+                from: path.resolve(__dirname, 'favicon.ico'),
+                to: path.resolve(__dirname, 'dist')
+            },
+            {
+                from: path.resolve(__dirname, 'src', 'images', 'img1.jpg'),
+                to: path.resolve(__dirname, 'dist')
+            },
+            {
+                from: path.resolve(__dirname, 'src', 'images', 'img3.jpg'),
+                to: path.resolve(__dirname, 'dist')
+            },
+            {
+                from: path.resolve(__dirname, 'src', 'images', 'img4.jpg'),
+                to: path.resolve(__dirname, 'dist')
+            },
+        ]),
+        new MiniCssExtractPlugin({
+            filename: filename('css')
+        })
+    ],
     module: {
         rules: [{
                 test: /\.css$/,
                 use: cssLoaders()
             },
             {
-                test: /\.(png|jpg|jpeg|svg)$/,
+                test: /\.s[ac]ss$/,
+                use: cssLoaders('sass-loader')
+            },
+            {
+                test: /\.(png|jpg|svg|gif)$/,
                 use: ['file-loader']
             },
             {
@@ -161,8 +192,7 @@ module.exports = {
                     loader: 'babel-loader',
                     options: babelOptions('@babel/preset-react')
                 }
-            },
-
+            }
         ]
     }
 }
