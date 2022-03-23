b32f4d69b7b4a780b25ab90279350060669fadbd
Tomamonomae <snabukraine@ukr.net>
2022-03-23 10:21:44 +0200

    Add helpers for API

---
+++ backend/services/updateUserBonus.js
@@ -0,0 +1,19 @@
+const User = require("../models/User");
+
+const updateUserBonus = async(id, total) => {
+
+    if (total >= 1000) {
+        let bonus = 50;
+        const user = await User.findById(id);
+
+        user.bonus = bonus + user.bonus;
+
+        user.save();
+
+        return user.bonus;
+    }
+
+    return;
+}
+
+module.exports = updateUserBonus;
