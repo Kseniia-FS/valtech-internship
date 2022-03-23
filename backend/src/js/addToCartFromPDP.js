a9343d33a34d34fd9be9cfc38b96eec2f0d19c07
Tomamonomae <snabukraine@ukr.net>
2022-03-23 10:33:55 +0200

    Fix adding of product

---
+++ backend/src/js/addToCartFromPDP.js
@@ -0,0 +1,50 @@
+import Notiflix from 'notiflix';
+
+const productPrice = document.querySelector("#pdpPrice");
+const productImage = document.querySelector("#pdpImage");
+const productTitle = document.querySelector("#pdpTitle");
+const button = document.querySelector("#addToCart");
+const quantityInput = document.querySelector("#quantity");
+const productIdElem = document.querySelector("#productID");
+
+
+
+const cart = window.localStorage;
+let orders = [];
+
+
+button.addEventListener("click", addToCart);
+
+function addToCart(e) {
+
+    const totalProductSum = Number(productPrice.textContent) * quantityInput.textContent;
+
+    const storage = cart.getItem("cart");
+
+    if (storage) {
+        const parsedStorage = JSON.parse(storage);
+
+
+        orders = [...parsedStorage];
+
+    }
+
+    const productId = productIdElem.href.split('=')[1];
+
+    const product = {
+        productUrl: window.location.href,
+        id: productId,
+        imgUrl: productImage.src,
+        title: productTitle.textContent,
+        price: Number(productPrice.textContent),
+        quantity: quantityInput.textContent,
+        total: totalProductSum,
+
+    };
+
+    orders.push(product);
+
+
+    cart.setItem("cart", JSON.stringify(orders));
+    Notiflix.Notify.success('Product successfully added to cart');
+}
